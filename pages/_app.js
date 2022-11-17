ed8fcc94876c79fa2572938bafde984af48c21a8
ratio0ktaha <tahahusain.mv@gmail.com>
2022-11-17 12:13:00 +0500

    foix

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
+<Head> 
+  git
+  
+  
+</Head>
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default MyApp 
