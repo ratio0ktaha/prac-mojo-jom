663151d5de7256f44d7c9fe6bf1d522831e1b801
ratio0ktaha <tahahusain.mv@gmail.com>
2022-11-23 17:46:15 +0500

    any

--- components/Feature2.js
+++ components/Feature2.js
@@ -1,4 +1,4 @@
-import stylus from "../styles/Feature2.module.css";
+import styles from "../styles/Feature2.module.css";
 import Buttons from "./Button";
 import main4 from "../public/images/main4.png";
 import Image from "next/image"
@@ -8,35 +8,29 @@ export default function Feature2() {
    
    
     return(
+        <>
 
+        <div className={styles.allin}>
+            <div className={styles.header}><h2>Data Sources that<br/> we support</h2></div>
+            <div className={styles.paragraph}><p>
+                
+            TopicMojo supports 50+ data sources. It's <br/>the easiest and most effective way to do topic research.
+                </p>
+             </div>
+              <Buttons className={styles.button}>
+               
+                press me 
 
+              </Buttons>
 
-    <div className={stylus.topics}>
-        <div className={stylus.flex_one}>
-          <div className={stylus.block}>
-            <h2>Data Sources that we support</h2>
-            <p>
-            TopicMojo supports 50+ data sources. It's the easiest and most effective way to do topic research.{" "}
-            </p>
-            <buttons>
-            <div className={stylus.buttons}>learn more
-            </div>
-              </buttons>
-          </div>
-          <div>
-            <Image className={stylus.img} src={main4} />
-          </div>
         </div>
+        
 
 
-
-
-
-
-
-
-
-
-
-        </div>)
+    <div className={styles.img}>
+    <Image className={styles.something} src={main4} />
+  </div>
+  </>
+    
+    )
 }
