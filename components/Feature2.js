4582d336ce79ddf2ff22ff2fe4c718dfd94a4f11
Taha hussain <tahahussain.mv@gmail.com>
2022-11-24 00:04:30 +0500

    changer

--- components/Feature2.js
+++ components/Feature2.js
@@ -1,36 +1,30 @@
 import styles from "../styles/Feature2.module.css";
-import Buttons from "./Button";
 import main4 from "../public/images/main4.png";
-import Image from "next/image"
-
+import Image from "next/image";
 
 export default function Feature2() {
-   
-   
-    return(
-        <>
-
-        <div className={styles.allin}>
-            <div className={styles.header}><h2>Data Sources that<br/> we support</h2></div>
-            <div className={styles.paragraph}><p>
-                
-            TopicMojo supports 50+ data sources. It's <br/>the easiest and most effective way to do topic research.
-                </p>
-             </div>
-              <Buttons className={styles.button}>
-               
-                press me 
-
-              </Buttons>
-
+  return (
+    <>
+      <div className={styles.allin}>
+        <div className={styles.header}>
+          <h2>
+            Data Sources that
+            <br /> we support
+          </h2>
         </div>
-        
+        <div className={styles.paragraph}>
+          <p>
+            TopicMojo supports 50+ data sources. It's <br />
+            the easiest and most effective way to do topic research.
+          </p>
+        </div>
+        <button className="press">Learn more
+        </button>
+      </div>
 
-
-    <div className={styles.img}>
-    <Image className={styles.something} src={main4} />
-  </div>
-  </>
-    
-    )
+      <div className={styles.img}>
+        <Image className={styles.something} src={main4} />
+      </div>
+    </>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,27 @@
-import Navbar from "../components/Navbar"
-import Hero from "../components/Hero"
-import Feature from "../components/Feature"
-import Icondisplay from "../components/Icondisplay"
-import Transition1 from "../components/Transition1"
-import Buttons from "../components/Button"
-import Cta from "../components/Cta"
-import Feature2 from"../components/Feature2"
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import Feature from "../components/Feature";
+import Icondisplay from "../components/Icondisplay";
+import Transition1 from "../components/Transition1";
+import Buttons from "../components/Button";
+import Cta from "../components/Cta";
+import Feature2 from "../components/Feature2";
+import Cards from "../components/Card";
 
 // import Cta from "../components/cta"
-
 
 export default function Home() {
   return (
     <div>
-
       {/* <Cta/> */}
-       <Navbar />
+      <Navbar />
       <Hero />
       <Feature />
-      <Icondisplay/>
+      <Icondisplay />
       <Transition1 />
-      <Cta/>
-      <Feature2/>
+      <Cta />
+      <Feature2 />
+      <Cards />
     </div>
-  )
+  );
 }
