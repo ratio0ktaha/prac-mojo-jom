89a0e17ef8e1e8fe481e8609b743d75583ee6047
ratio0ktaha <tahahusain.mv@gmail.com>
2022-12-01 15:15:51 +0500

    1dec

--- components/Longercards.js
+++ components/Longercards.js
@@ -7,18 +7,54 @@ import React from 'react'
 export default function Longercards() {
     return (
         <div className={styles.parental}>
-                <div className={styles}>
                 <div className={styles.output1}>
                   
-                <Image src={images} width={100} height={100}/>
-    
+                <Image className={styles.beach} src={images}/>
+                <h2 className={styles.a}>Starter</h2>
+                <p className={styles.b}>For Startups & SMEs</p>
+                <div className=".styles.mon">
+                <p className={styles.c}>$</p>
+                <p className={styles.d}>90</p>
+                <p className={styles.e}>billed yearly</p>
                 </div>
+                <button className={styles.f}><b>Chosse</b></button>
+                <div className={styles.words}>
+                <img src="https://d21b0h47110qhi.cloudfront.net/old-free/list-img1-mZlt5N3G4Gj3wd4.png"/>
+                    <div className={styles.tap}>50 queries/mo in Topic Model</div> 
+                    <img src="https://d21b0h47110qhi.cloudfront.net/old-free/list-img2-gSsDl9Tny1jBuBg.png"/>
+                    <div className={styles.tap}>50 queries/mo in Questions Finder</div>
+                    <img src="https://d21b0h47110qhi.cloudfront.net/old-free/list-img4-k5DKKhFAtPze2Td.png"/>
+                    <div className={styles.tap}>10 queries/mo in Outline</div>
+                    <img src="https://d21b0h47110qhi.cloudfront.net/old-free/list-img3-WSchKODD73CLIBF.png"/>
+                    <div className={styles.tap}>Free SEO Writer</div>
+                    <img src="https://d21b0h47110qhi.cloudfront.net/old-free/list-img5-vDyDj0PQhCk2Aj1.png"/>
+                    <div className={styles.tap}>CSV Exports</div>
+                    <img src="https://d21b0h47110qhi.cloudfront.net/old-free/list-imgcheck-kV7gbwkFy5vCmyQ.png"/>
+                    <div className={styles.tap}>Shareable Reports</div>
+                    <img src="https://d21b0h47110qhi.cloudfront.net/old-free/list-imgcheck-kV7gbwkFy5vCmyQ.png"/>
+                    <div className={styles.tap}>All countries supported</div>
+                    <img src="https://d21b0h47110qhi.cloudfront.net/old-free/list-imgcheck-kV7gbwkFy5vCmyQ.png"/>
+                    <div className={styles.tap}>SEO Data</div>
+                    <img src="https://d21b0h47110qhi.cloudfront.net/old-free/list-imgcheck-kV7gbwkFy5vCmyQ.png"/>
+                    <div className={styles.tap}>All data sources included</div>
+                    <img src="https://d21b0h47110qhi.cloudfront.net/old-free/list-imgcheck-kV7gbwkFy5vCmyQ.png"/>
+                    <div className={styles.tap}>Priority support</div>
+                    <img src="https://d21b0h47110qhi.cloudfront.net/old-free/list-imgcheck-kV7gbwkFy5vCmyQ.png"/>
+                    <div className={styles.tap}>Search listening</div>
+                    <img src="https://d21b0h47110qhi.cloudfront.net/old-free/list-imgcheck-kV7gbwkFy5vCmyQ.png"/>
+                    <div className={styles.tap}>Worksapces</div>
+                    <img src="https://d21b0h47110qhi.cloudfront.net/old-free/list-imgcross-labNDu3tfBFGqMS.png"/>
+                    <div className={styles.tap}>Team Members</div>
+                    <img src="https://d21b0h47110qhi.cloudfront.net/old-free/list-imgcross-labNDu3tfBFGqMS.png"/>
+                     <div className={styles.tap}>Custom domain</div>
+             </div>
+
+                </div>
+               
                   
-                        
-                </div>
-    
-            </div>
-    
+           
+          
+    </div>
             
     );
 }
